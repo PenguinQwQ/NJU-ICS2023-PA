930ed28b2fbc5c379482dd622a6c8092e654a181
tracer-ics2023 <tracer@njuics.org>
2023-11-25 18:43:03 +0800

    >  compile NEMU 211250177 黄俊荣 Linux hjr-virtual-machine 6.2.0-37-generic #38~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Nov  2 18:01:13 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  18:43:03 up 29 min,  1 user,  load average: 0.34, 0.48, 0.37

--- nemu/src/isa/riscv32/difftest/dut.c
+++ nemu/src/isa/riscv32/difftest/dut.c
@@ -17,8 +17,20 @@
 #include <cpu/difftest.h>
 #include "../local-include/reg.h"
 
+extern CPU_state cpu;
 bool isa_difftest_checkregs(CPU_state *ref_r, vaddr_t pc) {
-  return false;
+  for (int i = 0 ; i <= 31 ; i++)
+    if(ref_r->gpr[i] != cpu.gpr[i]) 
+      {
+        pc = cpu.pc;
+        return false;
+      }
+  if(ref_r->pc != cpu.pc) 
+    {
+      pc= cpu.pc;
+      return false;
+    }
+  return true;
 }
 
 void isa_difftest_attach() {
--- nemu/src/monitor/monitor.c
+++ nemu/src/monitor/monitor.c
@@ -34,6 +34,9 @@ static void welcome() {
   printf("For help, type \"help\"\n");
 }
 
+
+
+
 #ifndef CONFIG_TARGET_AM
 #include <getopt.h>
 
@@ -42,7 +45,193 @@ void sdb_set_batch_mode();
 static char *log_file = NULL;
 static char *diff_so_file = NULL;
 static char *img_file = NULL;
+static char *elf_file = NULL;
 static int difftest_port = 1234;
+
+#ifdef CONFIG_FTRACE
+unsigned char *strbuf = NULL;
+word_t strbuf_size = 0;
+word_t func_num = 0; 
+word_t *func_begin;
+word_t *func_end;
+word_t *func_name_index;
+
+
+
+static void load_elf()
+{
+  if(elf_file == NULL) return;
+  Assert(elf_file != NULL, "The elf_file name is an empty string!");
+  FILE *fp = fopen(elf_file, "r");
+  Assert(fp != NULL, "Failed to open '%s' elf file!", elf_file);
+
+  
+  fseek(fp, 0, SEEK_END);
+  long size = ftell(fp);
+  Log("The elf file is %s, size = %ld",elf_file, size);
+  fseek(fp, 0, SEEK_SET);
+
+
+  //Summon an elf_header
+  Elf32_Ehdr elf_header;
+  Assert(fread((void *)&elf_header, sizeof(Elf32_Ehdr), 1, fp) == 1, "Failed to read the elf header from elf file");
+  //Some basic checks on the elf file
+  Assert(elf_header.e_ident[EI_MAG0] == ELFMAG0,"Failed in checking the magic number");
+  Assert(elf_header.e_ident[EI_MAG1] == ELFMAG1,"Failed in checking the magic number");
+  Assert(elf_header.e_ident[EI_MAG2] == ELFMAG2,"Failed in checking the magic number" );
+  Assert(elf_header.e_ident[EI_MAG3] == ELFMAG3,"Failed in checking the magic number" );
+
+  Assert(elf_header.e_ident[EI_CLASS] == ELFCLASS32, "Wrong elf class!");
+  Assert(elf_header.e_ident[EI_DATA] == ELFDATA2LSB, "Should be ELFDATA2LSB!");
+  Assert(elf_header.e_ident[EI_VERSION] == EV_CURRENT, "Wrong on the elf version check");
+  Log("Pass All the Elf file test!");
+  Assert(fseek(fp, 0L, SEEK_SET) == 0, "Failed in fseek");
+
+ //char strbuf[65536]; //save the ftrace strable
+  Elf32_Off shdr_off = elf_header.e_shoff, shnum = elf_header.e_shnum, shsize = elf_header.e_shentsize;
+  for (int i = 0 ; i < shnum ; i++)
+  {
+    Elf32_Off sh_offset = shdr_off + shsize * i;
+    Assert(fseek(fp, 0, SEEK_SET) == 0, "Failed in fseek");
+    Assert(fseek(fp, sh_offset, SEEK_CUR) == 0, "Failed in fseek");
+    Elf32_Shdr shdr;
+    Assert(fread((void *)&shdr, sizeof(Elf32_Shdr), 1, fp) == 1, "Failed to read a shdr");
+    if(shdr.sh_type == SHT_STRTAB && (i != elf_header.e_shstrndx)) //Here we read the strtable
+      {
+    //    printf("index in shdr table %d, e_shstrndx is %d\n", i, elf_header.e_shstrndx);
+        Elf32_Off strtab_off = shdr.sh_offset, strtab_size = shdr.sh_size;
+        strbuf_size = strtab_size;
+        Log("Find SHT_STRTABLE!shdr.sh_offset = 0x%x, shdr.sh_size = 0x%x", strtab_off, strtab_size);
+        Assert(fseek(fp, 0, SEEK_SET) == 0, "Failed in fseek");
+        Assert(fseek(fp, strtab_off, SEEK_CUR) == 0, "Failed in fseek");
+        strbuf = (unsigned char *)malloc(strtab_size);
+        Assert(fread(strbuf, (size_t)strtab_size, 1, fp) == 1, "Failed to load the strtab section"); 
+        for (int pos = 0 ; pos <= strtab_size - 1 ; pos++)
+          putchar(strbuf[pos]);
+        putchar('\n');
+      }
+    /*
+      Process the symtable, construct pc->function_name pair and save it in the ftrace table
+      (begin, end) ---> function name(char * string)
+      (func_begin, func_begin + func_size) ---> func_name
+    */
+
+    if(shdr.sh_type == SHT_SYMTAB) 
+      {
+        Elf32_Off symtab_off = shdr.sh_offset, symtab_size = shdr.sh_size;
+        int symnum = symtab_size / sizeof(Elf32_Sym);
+        Assert(fseek(fp, 0L, SEEK_SET) == 0, "Failed in fseek");
+        Assert(fseek(fp, symtab_off, SEEK_CUR) == 0, "Failed in fseek");
+        func_begin = (word_t *)malloc(sizeof(word_t) * symnum);
+        func_end = (word_t *)malloc(sizeof(word_t) * symnum);     
+        //Alloc symnum unsigned char * to prepare for the function name load
+        func_name_index = (word_t *)malloc(sizeof(word_t) * symnum);
+        printf("Sym number is %d\n", symnum);
+        for (int j = 1 ; j <= symnum ; j++)
+          {
+            Elf32_Sym sym;
+            Assert(fread((void *)&sym, sizeof(Elf32_Sym), 1, fp) == 1, "Failed to read sym");
+            printf("sym.st_value is 0x%08x sym.st_info is 0x%08x sym.st_name is 0x%08x sym.st_size is 0x%08x\n", sym.st_value, sym.st_info, sym.st_name, sym.st_size);
+            if(ELF32_ST_TYPE(sym.st_info) == STT_FUNC)
+            {
+              printf("Find func type!\n");
+              func_begin[func_num] = sym.st_value;//begin address
+              func_end[func_num] = sym.st_value + sym.st_size - 4; //end address
+              func_name_index[func_num] = sym.st_name;
+              func_num++;
+            }
+          }
+      }
+  }
+  //Log("The strtab length: %ld, contents:\n %s", strlen(strtable), strtable);
+}
+
+char *find_func_by_index(word_t id)
+{
+  return (char *)(strbuf + id);
+}
+
+void display_all_function_info()
+{
+  printf("Function number is: %d", func_num);
+  for (int i = 0 ; i < func_num ; i++)
+      printf("Function: %s, Address: [0x%08x, 0x%08x]\n", find_func_by_index(func_name_index[i]), func_begin[i], func_end[i]);
+}
+
+static word_t stk_top = 0;
+static word_t func_call_stack[65536];
+static char *curr_run[65536];
+static int jmp_id, ret_id;
+static bool jmp_func, ret_func;
+
+void detect_and_display_function_call(word_t _now_pc, word_t jal_addr, word_t ival)
+{
+  jmp_func = false;
+  ret_func = false;
+  jmp_id = -1;
+  ret_id = -1;
+  for (int i = 0 ; i < func_num ; i++)
+    {
+      if(jal_addr == func_begin[i]) //jal to func_begin must means call!
+        {
+          jmp_func = true;
+          jmp_id = i;
+        }
+      if((_now_pc >= func_begin[i]) && (_now_pc <= func_end[i])) //means the tail will ret
+          ret_id = i;
+    }
+  if(ival == 0x00008067) //ret instruction value;
+    ret_func = true;
+
+//  if((stk_top > 0) && (func_call_stack[stk_top] == jal_addr)) ret_func = true; //means will ret to the last recursion 
+  if((jmp_func == false) && (ret_func == false)) return;//no ftrace display
+  if((jmp_func == true) && (ret_func == false)) //just call the next function, recursion stack grows!
+  { 
+    stk_top++;
+    printf("0x%08x:", _now_pc);
+    func_call_stack[stk_top] = _now_pc + 4;//static next pc is the return address
+    curr_run[stk_top] = find_func_by_index(func_name_index[jmp_id]);
+    for (int j = 1 ; j < stk_top ; j++) //construct recursion display
+        putchar(' ');
+    printf("call [%s@0x%08x]\n", curr_run[stk_top], jal_addr);
+    return;  
+  }
+  if((jmp_func == false) && (ret_func == true)) //just return from the current function
+  {
+    printf("0x%08x:", _now_pc);
+    for (int j = 1 ; j < stk_top ; j++) //construct recursion display
+        putchar(' ');
+    printf("ret [%s]\n", curr_run[stk_top]);
+    stk_top--;
+    return;
+  }
+  if((jmp_func == true) && (ret_func == true)) //tail recursion!
+  {
+    //First print the ret infomation
+    printf("0x%08x:", _now_pc);
+    for (int j = 1 ; j < stk_top ; j++) //construct recursion display
+        putchar(' ');
+    printf("ret [%s]\n", curr_run[stk_top]);
+    stk_top--;   
+    //Second print the call information
+    stk_top++;
+    printf("0x%08x:", _now_pc);
+    func_call_stack[stk_top] = _now_pc + 4;//static next pc is the return address
+    curr_run[stk_top] = find_func_by_index(func_name_index[jmp_id]);
+    for (int j = 1 ; j < stk_top ; j++) //construct recursion display
+        putchar(' ');
+    printf("call [%s@0x%08x]\n", curr_run[stk_top], jal_addr);
+    return;  
+  }
+  Assert(0, "Shouldn' reach here!");
+  return;//Not a function call;
+}
+
+
+
+
+#endif
+
 
 static long load_img() {
   if (img_file == NULL) {
@@ -73,6 +262,7 @@ static int parse_args(int argc, char *argv[]) {
     {"diff"     , required_argument, NULL, 'd'},
     {"port"     , required_argument, NULL, 'p'},
     {"help"     , no_argument      , NULL, 'h'},
+    {"elf"      , required_argument, NULL, 'e'},
     {0          , 0                , NULL,  0 },
   };
   int o;
@@ -82,6 +272,7 @@ static int parse_args(int argc, char *argv[]) {
       case 'p': sscanf(optarg, "%d", &difftest_port); break;
       case 'l': log_file = optarg; break;
       case 'd': diff_so_file = optarg; break;
+      case 'e': elf_file = optarg; break;
       case 1: img_file = optarg; return 0;
       default:
         printf("Usage: %s [OPTION...] IMAGE [args]\n\n", argv[0]);
@@ -119,6 +310,13 @@ void init_monitor(int argc, char *argv[]) {
 
   /* Load the image to memory. This will overwrite the built-in image. */
   long img_size = load_img();
+  
+  /* Load the elf file for ftrace */
+  #ifdef CONFIG_FTRACE
+    load_elf();
+    display_all_function_info();
+  #endif
+
 
   /* Initialize differential testing. */
   init_difftest(diff_so_file, img_size, difftest_port);
