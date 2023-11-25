ab24bb3caafd660ef5312f425fde0c888ea669d3
tracer-ics2023 <tracer@njuics.org>
2023-11-25 20:48:48 +0800

    >  compile NEMU 211250177 黄俊荣 Linux hjr-virtual-machine 6.2.0-37-generic #38~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Nov  2 18:01:13 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  20:48:48 up  2:35,  1 user,  load average: 0.25, 0.25, 0.24

--- nemu/scripts/native.mk
+++ nemu/scripts/native.mk
@@ -25,7 +25,7 @@ $(BINARY):: compile_git
 # Some convenient rules
 
 override ARGS ?= --log=$(BUILD_DIR)/nemu-log.txt
-override ARGS += $(ARGS_DIFF) --batch
+override ARGS += $(ARGS_DIFF)
 
 # Command to execute NEMU
 IMG ?=
