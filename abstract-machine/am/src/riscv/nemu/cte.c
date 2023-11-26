5c402be40f8cdc79faa452bc4f4e64daa97cf644
tracer-ics2023 <tracer@njuics.org>
2023-11-26 14:45:25 +0800

    >  compile NEMU 211250177 黄俊荣 Linux hjr-virtual-machine 6.2.0-37-generic #38~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Nov  2 18:01:13 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  14:45:25 up  2:00,  1 user,  load average: 0.71, 0.52, 0.35

--- abstract-machine/am/src/riscv/nemu/cte.c
+++ abstract-machine/am/src/riscv/nemu/cte.c
@@ -7,7 +7,7 @@ static Context* (*user_handler)(Event, Context*) = NULL;
 Context* __am_irq_handle(Context *c) {
   if (user_handler) {
     Event ev = {0};
-    printf("c->mcause is %x \n", c->mcause);
+    printf("c->mcause is %d \n", c->mcause);
     switch (c->mcause) {
 
       default: ev.event = EVENT_ERROR; break;
