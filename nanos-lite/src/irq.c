eece520375b0dc745a4f1f6fe3f080fcfae8e39f
tracer-ics2023 <tracer@njuics.org>
2023-11-26 16:44:24 +0800

    >  compile NEMU 211250177 黄俊荣 Linux hjr-virtual-machine 6.2.0-37-generic #38~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Nov  2 18:01:13 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  16:44:24 up  3:59,  1 user,  load average: 0.60, 0.45, 0.34

--- nanos-lite/src/irq.c
+++ nanos-lite/src/irq.c
@@ -1,7 +1,11 @@
 #include <common.h>
+#define YIELD_FLAG 1
 
-static Context* do_event(Event e, Context* c) {
+static Context* do_event(Event e, Context* c) {//The event handler functin
   switch (e.event) {
+    case YIELD_FLAG:
+      printf("Yield!\n");
+      break;
     default: panic("Unhandled event ID = %d", e.event);
   }
 
